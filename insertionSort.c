55e2ed4348f39a1d9cdd378fc65e247220b8d490
jrTanvirHasan27 <59051954+jrTanvirHasan27@users.noreply.github.com>
2021-09-01 22:09:06 +0600

    Add files via upload

---
+++ insertionSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+void insertionsort(int arr[], int n)
+{
+    int i, j, temp;
+
+    for(i=1; i<n; i++)
+    {
+        temp = arr[i];
+        j=i-1;
+
+        while(temp<arr[j] && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j=j-1;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+int main()
+{
+    int n,arr[1000],i,j,s=0;
+
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionsort(arr,n);
+    for(i=0; i<n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+}
